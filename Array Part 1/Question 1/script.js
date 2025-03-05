f266a42f9fb15d6b1aad70a106c11aa7acc636fb
Amritpal Bhinder <bhinderrajwinderkaur407@gmail.com>
2025-03-05 12:48:28 +0530

    Add files via upload

---
+++ Array Part 1/Question 1/script.js
@@ -0,0 +1,9 @@
+let arr= [1 , 5 , 22 , 98 , 65 , 34 , 100 , 63 , 72 , 18 , 48]
+
+for(let i=0; i<arr.length;i++)
+{
+    if(arr[i]%2==0)
+    {
+        document.write(arr[i] + " ")
+    }
+}
---
+++ Array Part 1/Question 2/script.js
@@ -0,0 +1,13 @@
+let arr= [1 , 23 , 44 , 67 , 66 , 78 , 92 , 57 , 37 , 88]
+
+for(let i=0; i<arr.length; i++)
+{
+    if(arr[i]%2==0)
+    {
+        document.write("1" + " ")
+    }
+    else
+    {
+        document.write("0" + " ")
+    }
+}
